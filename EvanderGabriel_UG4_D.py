5c41a05777421dbb578f830cc0ebbcba3c77fb1c
Evander Gabriel <93313423+evangab123@users.noreply.github.com>
2022-09-19 20:09:35 +0700

    Update EvanderGabriel_UG4_D.py
    revisi3

--- EvanderGabriel_UG4_D.py
+++ EvanderGabriel_UG4_D.py
@@ -8,17 +8,15 @@ with open('mahasiswa.json','r') as file:
         count = 1
         n_hobi = int(input('Masukan jumlah hobi : '))
         hobi_list =list()
+        bio = []
         for j in range(0,n_hobi):
             hobi = input('masukan hobi ke-'+str(count)+' : ')
             count += 1
             hobi_list.append(hobi)  
         prestasi = input("Masukan prestasi Anda : ")
-        data = {nama:[]}
-        biodata = list()
-        biodata.append({'Hobi': hobi_list})
-        biodata.append({'Prestasi':prestasi})
-        data[nama].append({'Biodata':biodata})
+        bio.append({"BioData":{"Hobi":hobi_list,"Prestasi":prestasi}})
+        data[nama] = bio
         print("=== Data berhasil ditambahkan ===\n")      
 
-with open('mahasiswa.json','a') as file:
+with open('mahasiswa.json','w') as file:
      data = json.dump(data,file)   
